8aa3b3f4dbdf66e75656f06fc99073ea45becafa
kerimovmurat <kerimovmurat@yandex.ru>
2024-07-04 07:24:21 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+kolichestvo_vipolnenuh_dz = 12
+kolichestvo_zatrachenuh_chasov = 1.5
+nazvanie_kursa = 'Python'
+vremya_na_odno_zadanie = kolichestvo_zatrachenuh_chasov / kolichestvo_vipolnenuh_dz
+print('Курс:',nazvanie_kursa,',','всего задач:',kolichestvo_vipolnenuh_dz,',','затрачено часов:',kolichestvo_zatrachenuh_chasov,',','среднее время выполнения:',vremya_na_odno_zadanie)
